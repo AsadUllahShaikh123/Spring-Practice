9d3f74ca424ca033e6b04da579fa1bcde779f604
AsadullahShaikh123 <asadullahshaikh713@gmail.com>
2023-09-23 12:30:21 +0500

    constructor injection

---
+++ src/main/java/com/constructor_injection/Main.java
@@ -0,0 +1,14 @@
+package com.constructor_injection;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+
+	public static void main(String [] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("com/constructor_injection/config.xml");
+		
+		Student student = context.getBean("std1",Student.class);
+		System.out.print(student);
+	}
+}
---
+++ src/main/java/com/constructor_injection/Student.java
@@ -0,0 +1,18 @@
+package com.constructor_injection;
+
+public class Student {
+
+	private int id;
+	private String name;
+	
+	public Student(int id, String name) {
+		
+		this.id = id;
+		this.name= name;
+	}
+	@Override
+	public String toString() {
+		return "Student [id=" + id + ", name=" + name + "]";
+	}
+	
+}
