a6f2ee2d5beb7f81c95ad973b45868427a6ad826
AsadullahShaikh123 <asadullahshaikh713@gmail.com>
2023-09-24 00:02:51 +0500

    auto wiring byName

---
+++ src/main/java/com/auto_wiring/Main.java
@@ -0,0 +1,14 @@
+package com.auto_wiring;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+
+	public static void main(String [] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("com/auto_wiring/config.xml");
+		
+		Student student = context.getBean("std1",Student.class);
+		System.out.println(student);
+	}
+}
