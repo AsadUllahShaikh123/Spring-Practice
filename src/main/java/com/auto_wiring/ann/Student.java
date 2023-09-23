029da1eb1a3e7bb25ee85d53c5a5ff40c80d152b
AsadullahShaikh123 <asadullahshaikh713@gmail.com>
2023-09-24 01:04:51 +0500

    autowiring by annotations (property)

---
+++ src/main/java/com/auto_wiring/ann/Student.java
@@ -0,0 +1,39 @@
+package com.auto_wiring.ann;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class Student {
+
+	private int id;
+	private String name;
+	@Autowired
+	private Address address;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		System.out.println("Id setter ");
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		System.out.println("Name setter ");
+		this.name = name;
+	}
+	public Address getAddress() {
+		return address;
+	}
+	public void setAddress(Address address) {
+		System.out.println("Addreess Setter");
+		this.address = address;
+	}
+	@Override
+	public String toString() {
+		return "Student [id=" + id + ", name=" + name + ", address=" + address + "]";
+	} 
+	
+	
+}
