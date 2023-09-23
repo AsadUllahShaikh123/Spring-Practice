d04da5005e28658c9da9a265c6c5a7e78ac29908
AsadullahShaikh123 <asadullahshaikh713@gmail.com>
2023-09-23 12:57:32 +0500

    completed constructor injection concepts

--- src/main/java/com/constructor_injection/Student.java
+++ src/main/java/com/constructor_injection/Student.java
@@ -1,30 +1,46 @@
 package com.constructor_injection;
 
+import java.util.List;
+
 public class Student {
 
 	private int id;
 	private String name;
 	private String address;
-	
-	public Student(int id, String name) {
-		System.out.println("Int , String");
+	private List<String> courses;
+	private Certificate certificate;
+
+//	public Student(int id, String name) {
+//		System.out.println("Int , String");
+//		this.id = id;
+//		this.name= name;
+//	}
+//	public Student(String name , int id) {
+//		System.out.println("String , INt");
+//		this.id= id;
+//		this.name = name;
+//	}
+//	public Student(String name, String address) {
+//		System.out.println("String, String");
+//		this.name = name;
+//		this.address = address;
+//	}
+
+	public Student(int id, String name, String address, List<String> courses, Certificate certificate) {
+		super();
 		this.id = id;
-		this.name= name;
-	}
-	public Student(String name , int id) {
-		System.out.println("String , INt");
-		this.id= id;
-		this.name = name;
-	}
-	public Student(String name, String address) {
-		System.out.println("String, String");
 		this.name = name;
 		this.address = address;
+		this.courses = courses;
+		this.certificate = certificate;
 	}
+
 	@Override
 	public String toString() {
-		return "Student [id=" + id + ", name=" + name + ", address=" + address + "]";
+		return "Student [id=" + id + ", name=" + name + ", address=" + address + ", courses=" + courses
+				+ ", certificate=" + certificate + "]";
 	}
+
 	
-	
+
 }
