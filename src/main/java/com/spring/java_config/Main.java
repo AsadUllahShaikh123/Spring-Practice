ba17a13444143e05b136a6492c3a9007704e37fd
AsadullahShaikh123 <asadullahshaikh713@gmail.com>
2023-09-24 17:15:42 +0500

    using @Component with the help of java config file

---
+++ src/main/java/com/spring/java_config/Main.java
@@ -0,0 +1,14 @@
+package com.spring.java_config;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+
+	public static void main(String[] args) {
+		ApplicationContext context = new AnnotationConfigApplicationContext(config.class);
+		Student student = context.getBean("student",Student.class);
+		
+		System.out.print(student);
+	}
+}
---
+++ src/main/java/com/spring/java_config/config.java
@@ -0,0 +1,10 @@
+package com.spring.java_config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.spring.java_config")
+public class config {
+
+}
