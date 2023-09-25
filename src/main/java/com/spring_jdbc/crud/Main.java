669176cc3194f8597632790d580f83b3505cac1f
AsadullahShaikh123 <asadullahshaikh713@gmail.com>
2023-09-26 02:09:17 +0500

    Fetched All Data from database with the help of anonymous RowMapper

--- src/main/java/com/spring_jdbc/crud/Main.java
+++ src/main/java/com/spring_jdbc/crud/Main.java
@@ -1,10 +1,15 @@
 package com.spring_jdbc.crud;
 
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+
+import com.spring_jdbc.Student;
 
 public class Main {
 
@@ -29,6 +34,38 @@ public class Main {
 //		int updated = jdbc.update(sql,"Asadullah","moro",1);
 //		
 //		System.out.print("updated : " + updated);
+		
+		// =========== Delete Operation ============ 
+//		
+//		String sql = " delete from student where id = ? ";
+//		int deleted = jdbc.update(sql,1);
+//		System.out.print("Deleted " + deleted);
+		
+		// Fetched All Data from the database 
+		
+//		String sql = " select * from student " ;
+//		
+		RowMapper<Student> rowMapper = new RowMapper<Student>() {
+
+			public Student mapRow(ResultSet rs, int rowNum) throws SQLException {
+				
+				Student student = new Student();
+				student.setId(rs.getInt("id"));
+				student.setName(rs.getString("name"));
+				student.setAddress(rs.getString("address"));
+				return student;
+			}
+			
+		};
+//		
+//		List<Student> students = jdbc.query(sql,rowMapper );
+//		for(Student student : students) {
+//		
+//		System.out.print(student.getId()+ " "  + student.getName() + " " + student.getAddress());
+//		System.out.println();		
+//		
+//		}
+		
 		
 	}
 }
